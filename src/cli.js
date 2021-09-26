ed7b227b3fee6253bb6f0c32c3e4891f3328686c
nickyxxxl <nickyswinckels@yahoo.com>
2021-09-26 10:47:35 +0200

    creditinal storage

--- src/cli.js
+++ src/cli.js
@@ -1,26 +1,52 @@
 import * as api from './main.js'
 import promp from 'prompt-sync'
+import fs from 'fs'
 
 const prompt = promp()
 
-//temp variables !!REMOVE BEFORE PUBLICATION!!
+//temp variables
 const schoolUUID = 'b6256752-bbcf-42e0-8c7c-9e4643f0e827';
-const _pwd = Buffer.from('QTFiMmMzZDQh', 'base64');
-const _usr = '4403';
 
-const username = prompt('user: ', _usr);
-const passwd = prompt.hide('pass: ');
+//check for stored login
+var creds;
+if (fs.existsSync('creds.txt')) {
+    creds = JSON.parse(fs.readFileSync('creds.txt','utf8'));
+    console.log('login loaded');
+}else{
+    console.log('no login stored');
+}
+
+//login prompt (if no login is stored)
+var username;
+var passwd;
+if (!creds) {
+    username = prompt('user: ');
+    passwd = prompt.hide('pass: ');
+
+    const save = prompt('remember login? [y/n]');
+    if (save == 'y') {
+        fs.writeFile('creds.txt', JSON.stringify({'user' : username, 'pass' : passwd}), (err) => {console.log(err);})
+        console.log('login saved');
+    } else if (save == 'n'){
+        console.log('not saving login');
+    } else {
+        console.log('invalid character, not saving login');
+    }
+} else {
+    username = creds.user;
+    passwd = creds.pass;
+}
 
 let tokens = await api.Authenticate(schoolUUID, username, passwd);
 
-const curDate = new Date(2021,9,19);
-let result = await api.GetSchedule(tokens.access_token,curDate,1);
+const curDate = new Date();
+let result = await api.GetSchedule(tokens.access_token,curDate,2);
 
 //filters and formats output
 function RoosterCLI(rawRooster) {
+    rawRooster.items.sort((a, b) => {return a.beginLesuur - b.beginLesuur})
     let ret = '';
-    rawRooster = rawRooster.items;
-    rawRooster.forEach(element => {
+    rawRooster.items.forEach(element => {
         const vak = element.titel.split(' - ')[1].split('.')[1];
         const body = `
         lesuur = ${element.beginLesuur}
